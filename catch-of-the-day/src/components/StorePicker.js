3c6f7c09acfce901008034c8303318af8603268f
perez6736 <perez6736@gmail.com>
2018-05-24 21:47:51 -0400

    css for react and creating new components

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,15 +1,13 @@
-import React, { Fragment, Component } from "react";
+import React, { Component } from "react";
 
 class StorePicker extends Component {
     render() {
         return (
-            <Fragment>
             <form className="store-selector">
                 <h2>Please Enter A Store.</h2>
                 <input type="text" required placeholder="Store Name"></input>
                 <button type="submit">Visit Store </button>
             </form>
-            </Fragment>
         )
     }
 }
