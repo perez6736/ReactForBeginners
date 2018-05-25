8504945a5dd0b671e4b6cb54de20f9d171417c28
perez6736 <perez6736@gmail.com>
2018-05-24 22:04:23 -0400

    css loaded correctly

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,10 +1,15 @@
 import React from "react"; 
+import Header from './Header'
 
 class App extends React.Component{
     render(){
         return (
             <div className="catch-of-the-day">
-                <p> heyy from app </p>
+                <div className="menu"> 
+                    <Header />
+                </div>
+                {/* <Inventory />
+                <Order /> */}
             </div>
         );
     }
